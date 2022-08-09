bcc6e955daec0f1ab56ad8696fc0ee61df54aef4
NathanJCai <nathancai1997@gmail.com>
2022-08-09 23:58:19 +1000

    Minor Update
    Updated UI to ask for a key press, and the prompt to disappear when a key has been pressed.
    Added a minor text shadow effect to all text
    Cleaned up the javascript to include switch statement rather than an if statement.

--- keycode.js
+++ keycode.js
@@ -2,12 +2,18 @@
 
 const key = document.querySelector("#key");
 const num = document.getElementById("num");
-let keyCode = document.addEventListener("keydown",(e)=>{
-    if (e.key ==" "){
-        key.textContent = "Space"
-    } else{
-        key.textContent= e.key;
+const enter = document.querySelector(".enter");
+const keyCode = document.querySelector("#keyCode");
+document.addEventListener("keydown",(e)=>{
+    switch(e.key){
+        case " ":
+            key.textContent = "Space"
+            break;
+        default:
+            key.textContent= e.key;
     }
+    enter.textContent = "";
+    keyCode.classList.add("border")
     num.textContent = e.keyCode;});
 
 
